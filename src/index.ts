c5c549522cbd1fc622ad93cf0954bd3072c8d39f
sue517 <csooa9305@gmail.com>
2021-02-15 17:59:11 +0900

    types commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// const name = "최수아",
+//     birth = "0517",
+//     gender = "female";
+
+const sayHi = (name:string, birth:number, gender:string): string => {
+    return `Hello ${name}, you are born in ${birth}, you are a ${gender}`;
+}
+
+console.log(sayHi('최수아', 517, 'female'));
+
+export {}; 
